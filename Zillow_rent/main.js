870e2a4509b9776443944d615d64926ca6057e38
Martina Gelnerová <martina.gelner.cz@gmail.com>
2022-04-04 11:03:54 +0200

    Zillow Rent

---
+++ Zillow_rent/main.js
@@ -0,0 +1,10 @@
+import Apify from 'apify';
+
+Apify.main(async () => {
+    const input = await Apify.getInput();
+
+    await Apify.metamorph('petr_cermak/zillow-api-scraper', {
+        ...input,
+        type: "rent"
+    });
+});
